1a3e58c9ff7af7a1a42dd48684ffbb12c188bbf0
Ajay Krishna <152839330+AjayKrishna-S@users.noreply.github.com>
2024-09-14 13:22:04 +0530

    Update connectMongo.js

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -1,5 +1,35 @@
 import mongoose from "mongoose";
 
-const connectMongo = async() => mongoose.connect(process.env.MONGO_URI) 
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectMongo() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export default connectMongo;
